a5e1f1f0b95c82345ca834f1b260fa02f63fc9f2
Yebin Lee <139226103+shail1027@users.noreply.github.com>
2026-05-06 13:25:31 +0900

    feat: 노트 채팅 이력 조회 API 구현

--- src/main/java/com/example/kuriq/repository/chat/ChatMessageRepository.java
+++ src/main/java/com/example/kuriq/repository/chat/ChatMessageRepository.java
@@ -1,9 +1,13 @@
 package com.example.kuriq.repository.chat;
 
 import com.example.kuriq.entity.chat.ChatMessage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, String> {
 
     void deleteByUserIdAndNoteId(String userId, String noteId);
+
+    Page<ChatMessage> findByUserIdAndNoteId(String userId, String noteId, Pageable pageable);
 }
