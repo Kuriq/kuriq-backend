34fcc73abffdd8df7bffd2e2c70ab8ac703cf6e8
hyunjung26 <154662473+hyunjung26@users.noreply.github.com>
2026-06-08 15:15:58 +0900

    fix: CORS Vercel 도메인 추가

--- src/main/java/com/example/kuriq/config/SecurityConfig.java
+++ src/main/java/com/example/kuriq/config/SecurityConfig.java
@@ -76,7 +76,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+                                        "https://kuriq-frontend.vercel.app"
+                                        ));
         config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
